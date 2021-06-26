a6bb5f8a30e5184dfac5fc7729acd8569272dc52
Yucky <registyucky@gmail.com>
2021-06-26 19:00:30 +0900

    import

--- scraper.py
+++ scraper.py
@@ -6,3 +6,17 @@ import requests
 import slackweb
 import pandas as pd
 from bs4 import BeautifulSoup
+
+def scraping():
+    url = 'https://qiita.com/ryo-futebol/items/235c212fdfc3704b7e9c'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    result = []
+    for top_news in soup.find_all(class_='取得したい箇所のクラス名'):
+        result.append([
+            top_news.text,
+            top_news.get('href')
+        ])
+    return result
+
+scraping()
