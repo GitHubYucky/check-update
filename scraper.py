986fe16351deabada529a0b1c4a14f83aece839e
Yucky <registyucky@gmail.com>
2021-06-26 19:24:24 +0900

    add sraping argument

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@ import slackweb
 import pandas as pd
 from bs4 import BeautifulSoup
 
-def scraping():
+def scraping(url):
     url = 'https://news.yahoo.co.jp/'
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
@@ -24,6 +24,8 @@ def output_csv(result):
     headers=['Title','URL']
     writer=csv.writer(file)
     writer.writerow(headers)
+    for row in result:
+      writer.writerow(row)
 
 def read_csv():
     if not os.path.exists('last_log.csv'):
@@ -33,5 +35,15 @@ def read_csv():
     csv_list = pd.read_csv('last_log.csv', header=None).values.tolist()
     return csv_list
 
+def list_diff(result, last_result):
+    return_list = []
+    for tmp in result:
+        if tmp not in last_result:
+            return_list.append(tmp)
+    return return_list
+
 result=scraping()
-output_csv(result)
+# output_csv(result)
+last_result=read_csv()
+diff_list=list_diff(result,last_result)
+print(diff_list)
